fd44b870f8d05e6b6bb235a4c2306e3594e93542
haudhi <haudhiizza@gmail.com>
2022-04-12 08:35:43 +0700

    adding fitur user

--- delivery/routes/router.go
+++ delivery/routes/router.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	_userHandler "project3/delivery/handler/users"
+	_middlewares "project3/delivery/middlewares"
+
+	"github.com/labstack/echo/v4"
+)
+
+func RegisterUserPath(e *echo.Echo, uh _userHandler.UserHandler) {
+	e.GET("/users", uh.GetUserByIdHandler(), _middlewares.JWTMiddleware())
+	e.POST("/users", uh.CreateUserHandler())
+	e.PUT("/users/:id", uh.UpdateUserHandler(), _middlewares.JWTMiddleware())
+	e.DELETE("/users/:id", uh.DeleteUserHandler(), _middlewares.JWTMiddleware())
+}
--- repository/user/interface.go
+++ repository/user/interface.go
@@ -1,1 +1,12 @@
 package user
+
+import (
+	_entities "project3/entities"
+)
+
+type UserRepositoryInterface interface {
+	GetUserById(id int) (_entities.User, int, error)
+	CreateUser(request _entities.User) (_entities.User, error)
+	UpdateUser(request _entities.User) (_entities.User, int, error)
+	DeleteUser(id int) error
+}
