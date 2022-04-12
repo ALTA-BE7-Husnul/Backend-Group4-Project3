fcacc7b9af4a4232800ebaf721bd3f64b680eb0e
husnulnawafil <husnulnawafil27@gmail.com>
2022-04-12 08:11:56 +0700

    add login feature

--- delivery/routes/router.go
+++ delivery/routes/router.go
@@ -1,1 +1,11 @@
 package routes
+
+import (
+	"project3/delivery/handler/auth"
+
+	"github.com/labstack/echo/v4"
+)
+
+func RegisterAuthPath(e *echo.Echo, ah *auth.AuthHandler) {
+	e.POST("/auth", ah.LoginHandler())
+}
