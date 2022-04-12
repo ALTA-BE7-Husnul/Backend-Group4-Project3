db252c93e037d2a7aeafcefab3caf79a75e8040c
haudhi <haudhiizza@gmail.com>
2022-04-12 22:36:46 +0700

    fixing deploy yml

--- entities/user.go
+++ entities/user.go
@@ -7,4 +7,5 @@ type User struct {
 	Name     string `gorm:"not null" json:"name" form:"name"`
 	Email    string `gorm:"not null;unique" json:"email" form:"email"`
 	Password string `gorm:"not null" json:"password" form:"password"`
+	Comment  []Comment `gorm:"foreignKey:UserID;references:ID"`
 }
