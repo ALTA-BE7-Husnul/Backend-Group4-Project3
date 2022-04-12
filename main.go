169ea931017407cf94f17447703265de65a92258
husnulnawafil <husnulnawafil27@gmail.com>
2022-04-12 10:33:32 +0700

    add routes for auth

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	_routes "project3/delivery/routes"
 	_authRepository "project3/repository/auth"
 	_authUseCase "project3/usecase/auth"
-	"project3/utils"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -20,7 +19,6 @@ import (
 	_userRepository "project3/repository/user"
 	_userUseCase "project3/usecase/user"
 
-	_routes "project3/delivery/routes"
 	_utils "project3/utils"
 )
 
@@ -28,10 +26,14 @@ func main() {
 	config := configs.GetConfig()
 	db := _utils.InitDB(config)
 
+	authRepo := _authRepository.NewAuthRepository(db)
+	authUseCase := _authUseCase.NewAuthUseCase(authRepo)
+	authHandler := _authHandler.NewAuthHandler(authUseCase)
+
 	userRepo := _userRepository.NewUserRepository(db)
 	userUseCase := _userUseCase.NewUserUseCase(userRepo)
 	userHandler := _userHandler.NewUserHandler(userUseCase)
-	
+
 	e := echo.New()
 	e.Use(middleware.CORS())
 	e.Pre(middleware.RemoveTrailingSlash())
@@ -40,8 +42,9 @@ func main() {
 		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
 	}))
 	e.Use(_middleware.CustomLogger())
-	
+
 	_routes.RegisterUserPath(e, userHandler)
+	_routes.RegisterAuthPath(e, authHandler)
 
 	log.Fatal(e.Start(fmt.Sprintf(":%v", config.Port)))
 }
