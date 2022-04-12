0b718131ce81ddd561ca759eaed8e45bddc8847f
husnulnawafil <husnulnawafil27@gmail.com>
2022-04-13 00:31:09 +0700

    initiate event endpoint

--- repository/event/event.go
+++ repository/event/event.go
@@ -1,1 +1,23 @@
 package event
+
+import (
+	_entities "project3/entities"
+
+	"gorm.io/gorm"
+)
+
+type EventRepository struct {
+	DB *gorm.DB
+}
+
+func NewEventRepository(db *gorm.DB) *EventRepository {
+	return &EventRepository{
+		DB: db,
+	}
+}
+
+func (er *EventRepository) GetEvents() ([]_entities.Event, error) {
+	var events []_entities.Event
+
+	return events, nil
+}
